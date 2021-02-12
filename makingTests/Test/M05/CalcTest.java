da40b367906821d21f478c262b6f6e9d8183b914
joseZaq <jose.zaquinaula.7e4@itb.cat>
2021-02-12 12:00:40 +0100

    multTest: passed :D

--- makingTests/Test/M05/CalcTest.java
+++ makingTests/Test/M05/CalcTest.java
@@ -1,26 +1,26 @@
 package M05;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
-    Calc calc=new Calc();
-    @org.junit.jupiter.api.Test
-    void suma() {
-        assertEquals(4,calc.suma(2,2));
+    Calc calc = new Calc();
+    @Test
+    void testSuma() {
+        assertEquals(2,calc.suma(1,1));
     }
-
-    @org.junit.jupiter.api.Test
-    void mult() {
-        assertEquals(4,calc.mult(2,2));
+    @Test
+    void testResta() {
+        assertEquals(2,calc.resta(3,1));
     }
-
-    @org.junit.jupiter.api.Test
-    void div() {
-        assertEquals(1,calc.div(2,2));
+    @Test
+    void testMult() {
+        assertEquals(6,calc.mult(3,2));
+        assertEquals(6,calc.mult(-3,-2));
+        assertEquals(-6,calc.mult(3,-2));
+        assertEquals(-6,calc.mult(-3,2));
     }
-
-    @org.junit.jupiter.api.Test
-    void res() {
-        assertEquals(0,calc.res(2,2));
+    @Test
+    void testDiv() {
+        assertEquals(2,calc.div(8,4));
     }
 }
--- makingTests/src/M05/Calc.java
+++ makingTests/src/M05/Calc.java
@@ -1,16 +1,25 @@
 package M05;
 
 public class Calc {
-    public int suma(int a,int b){
-        return a+b;
+    public int suma(int a, int b){
+        return (a+b);
     }
-    public int mult(int a,int b){
-        return a*b;
+    public int resta(int a, int b){
+        return (a-b);
     }
-    public int div(int a,int b){
-        return a/b;
+    public int mult(int a, int b){
+        //return (a*b);
+        int result = 0;
+        if(b < 0 ){
+            b*=-1;
+            a*=-1;
+        }
+        for (int i = 0; i < b; i++) {
+                result += a;
+        }
+        return result;
     }
-    public int res(int a,int b){
-        return a-b;
+    public int div(int a, int b){
+        return (a/b);
     }
 }
