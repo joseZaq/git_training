09de2cb21005cd4b9f0d22c89b2a81e983eaa897
josezaq <jose.zaquinaula.7e4@itb.cat>
2021-03-03 11:25:02 +0100

    makingTest 2

--- makingTests/Test/M05/EquipTest.java
+++ makingTests/Test/M05/EquipTest.java
@@ -1,11 +1,15 @@
 package M05;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EquipTest {
+    @Test
     void getIdTest(){
         Equip equip = new Equip(1,"Barcelona","BCN");
         assertEquals(1,equip.getId());
     }
+
 
 }
